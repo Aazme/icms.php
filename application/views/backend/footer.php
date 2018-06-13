3710c3966ccc55aaa86a074ca83c27d29eae6cb4
Abdulrahman Azmy <aazmy@adam.ai>
2018-06-13 14:51:10 +0200

    Uploading the App

---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2017 <strong>Bayanno Hospital Management System | Version 3.8</strong>. 
+    Developed by
+	<a href="http://creativeitem.com"
+    	target="_blank">Creativeitem</a>
+</footer>
